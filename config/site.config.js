b8446196f9510f4f4a7a400f7a5c0793d4e0642a
zultan-m <102913536+zultan-m@users.noreply.github.com>
2024-09-24 15:17:11 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Chiheisen Vercel Index",
+  title: "Encik TV",
 
   // The folder that you are to share publicly with Chiheisen-Vercel-Index. Use '/' if you want to share your root folder.
   baseDirectory: '/EncikZultan',
